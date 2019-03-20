70f1919a22ed876da9df6cf4430bde2dc47efd6d
Bharat Mhaskar <bmhaskar@gmail.com>
2019-03-21 01:39:51 +0530

    Updates

---
+++ precache-manifest.efcc91a87ea9b879e335a190159cb3bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "110ad185a385de121d6e",
+    "url": "/react-lists/static/js/main.eb2651a1.chunk.js"
+  },
+  {
+    "revision": "2ded2e204ca8513a71b6",
+    "url": "/react-lists/static/js/runtime~main.e5ab889b.js"
+  },
+  {
+    "revision": "c075899f9e937748d6ac",
+    "url": "/react-lists/static/css/2.4b16f30a.chunk.css"
+  },
+  {
+    "revision": "c075899f9e937748d6ac",
+    "url": "/react-lists/static/js/2.3bb44578.chunk.js"
+  },
+  {
+    "revision": "8960089ea554be67c87d3e379649ed51",
+    "url": "/react-lists/index.html"
+  }
+];
